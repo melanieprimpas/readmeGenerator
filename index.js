f6d3c61e819dc65539463b529cbebf55966ef33a
melanieprimpas <melanie.primpas@gmail.com>
2024-08-29 13:32:30 -0400

    Finished readme generator by adding badges for license

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import fs from 'fs';
 
 // TODO: Create an array of questions for user input
 const questions = ['What is the title of your project?', 'What is the description of your project?', 'Please enter your installation instructions:', 'Please provide instructions and examples for use: ', 'Please provide contribution guidelines:', 'Please provide test instructions: ', 'Please choose a license for your application from the list of options below:','Please enter your GitHub Username:','Please enter your email address:'];
+let licenseColor = '';
 
 inquirer
   .prompt([
@@ -55,10 +56,44 @@ inquirer
     }
   ])
   .then((response) => {
+
+    switch(response.license) {
+      case 'Apache License 2.0':
+        licenseColor = 'blue';
+        break;
+      case 'GNU General Public License v3.0':
+        licenseColor = 'green';
+        break;
+      case 'MIT License':
+        licenseColor = 'orange';
+        break;
+      case 'BSD 2-Clause "Simplified" License':
+        licenseColor = 'lightblue';
+        break;
+      case 'BSD 3-Clause "New" or "Revised" License':
+        licenseColor = 'yellow';
+        break;
+      case 'Boost Software License':
+        licenseColor = 'red';
+        break;
+      case 'Eclipse Public License 2.0':
+        licenseColor = 'purple';
+        break;
+      case 'Mozilla Public License 2.0':
+        licenseColor = 'brightgreen';
+        break;
+      
+      // Add cases for other license options and their respective colors
+      default:
+        licenseColor = 'blue'; // Default color
+    }
     const userTemplate = fillTemplate(response);
     writeToFile(userTemplate);
-
   });
+
+
+
+  
 
 // TODO: Create a function to write README file
 
@@ -74,7 +109,7 @@ function writeToFile(readmeString) {
 }
 
 function fillTemplate(response) {
-    return  `# ${response.title}\n 
+    return  `# <div style="display: flex; justify-content: space-between;"><div>${response.title}</div><div>![License: ${response.license}](https://img.shields.io/badge/License-${response.license.replace(/\s/g, '_')}-${licenseColor})</div></div>
 ## Description 
 ${response.description} \n
 ## Table of Contents
